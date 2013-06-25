a4ab0293bd33b0979e16310e71c8aab46fe6510c
twonegatives <twonegatives@twonegatives.(none)>
2013-06-25 21:41:33 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onlinefighter::Application.config.secret_token = '5f8f1eac6c9704a82c047e1598fdaa6bb13907106f8887e1bc063e201c1b6f328441aff74c96afd9774e70944d204f35c0a860f77ffac0ae169eff137df64c22'
